92f9d60cffd08ea2b3baf14e440660876c4518bf
Marwam <marwamprog.js@gmail.com>
2023-03-17 19:23:09 -0300

    Projeto Iniciado. - Criado o array de frases e criado a action do button NOVA FRASE para alteração de frases.

---
+++ app/src/main/java/com/cursoandroid/frasesdodia/MainActivity.java
@@ -0,0 +1,34 @@
+package com.cursoandroid.frasesdodia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovaFrase(View view) {
+
+        String[] frases = {
+                "Frase1",
+                "Frase2",
+                "Frase3",
+                "Frase4"
+        };
+
+        int numero = new Random().nextInt(4);
+
+        TextView texto = findViewById(R.id.textResultado);
+        texto.setText(frases[numero]);
+    }
+
+}
